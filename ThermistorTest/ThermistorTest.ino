c9b555b73ec73b4e9eeae82c1122db7a63141f2a
Peter Mitrano <mitranopeter@gmail.com>
2018-01-01 17:52:47 -0500

    thermistor test "works"
     - if I understand correctly, R_Ref is calculated to be 1.1k
     - works for the one sample (Temp, ADC) I have
     - removed the 1k resistor on the relay circuit

--- ThermistorTest/ThermistorTest.ino
+++ ThermistorTest/ThermistorTest.ino
@@ -1,3 +1,17 @@
+// Include math library for calculations
+#include <math.h>
+
+// Define ADC parameters
+#define ADC_TOP 1023
+
+// Define thermistor parameters
+#define R_NTC 10000
+#define BETA 3977
+#define LNA -4.12858298874828
+
+// Define Reference Resistor
+#define R_REF 1100
+
 const unsigned int enable = D2;
 const unsigned int led = D0;
 
@@ -10,7 +24,14 @@ void setup() {
 }
 
 void loop() {
-  unsigned int r = analogRead(A0);
-  Serial.println(r);
+  unsigned int raw = analogRead(A0);
+
+  double x = (R_REF * raw) / (ADC_TOP - raw);
+  x = BETA / (log(x) - LNA);
+  x = x - 273.15;
+
+  Serial.print(raw);
+  Serial.print(" ");
+  Serial.println(x);
   delay(1000);
 }
