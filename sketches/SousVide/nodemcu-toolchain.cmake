ae4c0b0f78e4989695f4902013a92b150956213c
Peter Mitrano <mitranopeter@gmail.com>
2018-01-05 10:12:07 -0500

    moved arduino code

---
+++ sketches/SousVide/nodemcu-toolchain.cmake
@@ -0,0 +1,3 @@
+set (TOOLCHAIN_ROOT /home/peter/.arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2/bin)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_ROOT}/g++)
+set(CMAKE_C_COMPILER ${TOOLCHAIN_ROOT}/gcc)
